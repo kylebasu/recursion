e07fdb94069fccddb46cd7d27b4eeaf04fa18a24
Kyle <kylebasu@gmail.com>
2015-06-07 22:31:42 -0700

    Implemented StringifyJSON funciton

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	if(typeof obj == 'string'){
+		return '"' + obj + '"';
+	}
+	if(obj == null){
+		return 'null';
+	}
+	if (typeof obj != 'object'){
+		return obj.toString();
+	}
+	if (Array.isArray(obj)){
+		var newStr = ""
+		for ( var i = 0; i < obj.length; i++){
+			if ( i == obj.length - 1){
+				newStr = newStr + stringifyJSON(obj[i])
+			}else{
+				newStr = newStr + stringifyJSON(obj[i]) + ','
+			}
+		};
+		newStr = '[' + newStr + ']'
+		return newStr
+	}
+	var newString = ''
+	var objProp = Object.keys(obj)
+
+	for ( var i = 0; i < objProp.length; i++){
+		var propVal = obj[objProp[i]]
+		var objPropToString = '"' + objProp[i] + '":'
+		if (typeof propVal == ('function' || 'undefined')){
+			return '{}';
+		}
+		if ( i == objProp.length - 1){
+				newString = newString + objPropToString + stringifyJSON(obj[objProp[i]])
+			}else{
+				newString = newString + objPropToString + stringifyJSON(obj[objProp[i]]) + ','
+			}
+	};
+	newString = '{' + newString + '}'
+	return newString
 };
